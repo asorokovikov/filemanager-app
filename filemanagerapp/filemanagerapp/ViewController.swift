38960376bf5bb88e5910d474940a9e13cac58365
Artem <asorokovikov@gmail.com>
2022-02-23 21:44:32 +0300

    Add Xcode project

---
+++ filemanagerapp/filemanagerapp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  filemanagerapp
+//
+//  Created by Artem Sorokovikov on 23.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
